5a03ef1f6160f0b5389eed9c361473b12d5f5cdb
bomeiweiwei <bomeilonch0210@gmail.com>
2025-08-22 14:40:34 +0800

    add shop service

---
+++ MyApiProject/Controllers/ShopsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopsController : ControllerBase
+    {
+        // GET: api/values
+        [HttpGet]
+        [Route("GetShops")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Shop1", "Shop2" };
+        }
+    }
+}
+
