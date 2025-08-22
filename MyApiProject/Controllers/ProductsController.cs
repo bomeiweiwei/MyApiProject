ba51db8b01328efbcb506178b4897a3ed4ca2bd7
bomeiweiwei <bomeilonch0210@gmail.com>
2025-08-22 14:37:48 +0800

    add products service

---
+++ MyApiProject/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET: api/values
+        [HttpGet]
+        [Route("GetProducts")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Product1", "Product2" };
+        }
+    }
+}
+
