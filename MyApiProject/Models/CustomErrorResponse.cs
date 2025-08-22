dfd90206fd65c773ea06347ab52d4b4da90f6339
bomeiweiwei <bomeilonch0210@gmail.com>
2025-08-22 11:56:16 +0800

    filter jwt auth

---
+++ MyApiProject/Models/CustomErrorResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace MyApiProject.Models
+{
+    public class CustomErrorResponse
+    {
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+
+        public CustomErrorResponse(string message, int statusCode = StatusCodes.Status401Unauthorized)
+        {
+            Message = message;
+            StatusCode = statusCode;
+        }
+    }
+}
+
--- MyApiProject/Program.cs
+++ MyApiProject/Program.cs
@@ -1,11 +1,38 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Plz input JWT Token，format like：Bearer {your token}"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
